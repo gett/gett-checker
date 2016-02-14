b0d149fed3e3b1381316169e91c7870f9d46ee17
thalaz <taras.halaz@gmail.com>
2016-02-14 19:58:39 +0200

    archive unpacker

--- donwloader.js
+++ donwloader.js
@@ -9,14 +9,16 @@ var known_archives = [
     'tar',
     'tar.gz',
     'zip',
-    //'7z',
-    //'rar'
-]
+    '7z', // p7zip-full
+    'rar',
+    'cpgz',
+    'tgz'
+];
 
 function Downloader(folder){
 
     var download_folder = folder;
-
+    var archiveType = undefined;
     this.downloadFile = function(file){
         return new Promise(function(resolve, reject){
             var args = " -L -o :path:filename :fileurl";
@@ -51,17 +53,45 @@ function Downloader(folder){
     };
 
     var unpack = function(file){
-
-    }
+        var filepath = download_folder + file.sharename + '/' + file.fileid;
+        var dest_dir = file.filename;
+        dest_dir = dest_dir.split('.');
+        dest_dir.pop();
+        dest_dir = dest_dir.join('');
+        dest_dir = filepath + '/' + dest_dir;
+        !fs.existsSync(dest_dir) && fs.mkdirSync(dest_dir);
+        var execQuery = '';
+        switch(archiveType) {
+            case 'tar':
+            case 'tar.gz':
+            case 'tgz':
+                execQuery = 'tar -xf ' + filepath + '/' + file.filename + ' -C ' + dest_dir; // tar -xf *.tar.gz -C temp/sharename/fileid/archName
+                break;
+            case 'zip':
+                execQuery = 'unzip -o ' + filepath + '/' + file.filename + ' -d ' + dest_dir; // unzip -o zip.zip -d temp/zip.zip
+                break;
+            case '7z':
+                execQuery = '7z x ' + filepath + '/' + file.filename + ' -y -o' + dest_dir; // 7z x 7z.7z -y -otemp/7z.7z
+                break;
+            case 'rar':
+                execQuery = 'unrar x ' + filepath + '/' + file.filename + ' ' + dest_dir; // unrar x rar.rar temp/rar.rar
+                break;
+        }
+        exec(execQuery, function(err, stdout, stderr) {
+            console.log(err, stdout, stderr);
+        });
+    };
 
     var isArchive = function(file){
         var isArchive = false;
         known_archives.every(function(type){
             isArchive = file.filename.indexOf(type, this.length - type.length) !== -1;
+            if(isArchive)
+                archiveType = type;
             return !isArchive;
         });
         return isArchive;
-    }
+    };
 
 }
 
