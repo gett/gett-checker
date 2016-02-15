d377d7619ee0704eb9b69a99ee14f7d16f9d3dd0
thalaz <taras.halaz@gmail.com>
2016-02-15 15:08:54 +0200

    downloader -> added promise to unpack method

--- donwloader.js
+++ donwloader.js
@@ -6,7 +6,7 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 
 var known_archives = [
-    'tar.gz',
+    'tar.gz', // should be before tar
     'tar',
     'zip',
     '7z', // p7zip-full
@@ -41,10 +41,13 @@ function Downloader(folder){
                 file.path = download_folder + file.filename;
                 if(!isArchive(file))
                     return resolve(file);
-                unpack(file);
+                unpack(file).then(function(file) {
+                    return resolve(file);
+                }).catch(function(stderr) {
+                    return reject(stderr);
+                });
                 //Unpacking archive
                 //Skip archives with password
-                resolve(file);
 
                 //Unpacking archives
 
@@ -54,43 +57,46 @@ function Downloader(folder){
     };
 
     var unpack = function (file){
-        var filepath = download_folder + file.sharename + '/' + file.fileid;
-        var dest_dir = file.filename;
-        dest_dir = dest_dir.split('.');
-        dest_dir.pop();
-        dest_dir = dest_dir.join('');
-        dest_dir = filepath + '/' + dest_dir;
-        !fs.existsSync(dest_dir) && fs.mkdirSync(dest_dir);
-        var execQuery = '';
-        switch(archiveType) {
-            case 'tar.gz':
-                var unpackedTar = file.filename.split('.');
-                unpackedTar.pop();
-                unpackedTar = unpackedTar.join('.');
-                execQuery = 'gzip -d ' + filepath + '/' + file.filename + ' && tar -xf ' + filepath + '/' + unpackedTar + ' -C ' + dest_dir;
-                // query example: gzip -d ./temp/8/0/targz.tar.gz && tar -xf ./temp/8/0/targz.tar -C ./temp/8/0/targztar
-                break;
-            case 'tar':
-            case 'tgz':
-                execQuery = 'tar -xf ' + filepath + '/' + file.filename + ' -C ' + dest_dir;
-                // query example: tar -xf ./temp/9/0/tgz.tgz -C ./temp/9/0/tgz
-                break;
-            case 'zip':
-                execQuery = 'unzip -o ' + filepath + '/' + file.filename + ' -d ' + dest_dir;
-                // query example: unzip -o ./temp/10/0/zip.zip -d ./temp/10/0/zip
-                break;
-            case '7z':
-                execQuery = '7z x ' + filepath + '/' + file.filename + ' -y -o' + dest_dir;
-                // query example: 7z x ./temp/11/0/7z.7z -y -o./temp/11/0/7z
-                break;
-            case 'rar':
-                execQuery = 'unrar x ' + filepath + '/' + file.filename + ' ' + dest_dir;
-                // query example: unrar x ./temp/12/0/rar.rar ./temp/12/0/rar
-                break;
-        }
-        exec(execQuery, function(err, stdout, stderr) {
-            if(err !== null)
-                console.log(err, stderr);
+        return new Promise(function(resolve, reject) {
+            var filepath = download_folder + file.sharename + '/' + file.fileid;
+            var dest_dir = file.filename;
+            dest_dir = dest_dir.split('.');
+            dest_dir.pop();
+            dest_dir = dest_dir.join('');
+            dest_dir = filepath + '/' + dest_dir;
+            !fs.existsSync(dest_dir) && fs.mkdirSync(dest_dir);
+            var execQuery = '';
+            switch(archiveType) {
+                case 'tar.gz':
+                    var unpackedTar = file.filename.split('.');
+                    unpackedTar.pop();
+                    unpackedTar = unpackedTar.join('.');
+                    execQuery = 'gzip -d ' + filepath + '/' + file.filename + ' && tar -xf ' + filepath + '/' + unpackedTar + ' -C ' + dest_dir;
+                    // query example: gzip -d ./temp/8/0/targz.tar.gz && tar -xf ./temp/8/0/targz.tar -C ./temp/8/0/targztar
+                    break;
+                case 'tar':
+                case 'tgz':
+                    execQuery = 'tar -xf ' + filepath + '/' + file.filename + ' -C ' + dest_dir;
+                    // query example: tar -xf ./temp/9/0/tgz.tgz -C ./temp/9/0/tgz
+                    break;
+                case 'zip':
+                    execQuery = 'unzip -o ' + filepath + '/' + file.filename + ' -d ' + dest_dir;
+                    // query example: unzip -o ./temp/10/0/zip.zip -d ./temp/10/0/zip
+                    break;
+                case '7z':
+                    execQuery = '7z x ' + filepath + '/' + file.filename + ' -y -o' + dest_dir;
+                    // query example: 7z x ./temp/11/0/7z.7z -y -o./temp/11/0/7z
+                    break;
+                case 'rar':
+                    execQuery = 'unrar x ' + filepath + '/' + file.filename + ' ' + dest_dir;
+                    // query example: unrar x ./temp/12/0/rar.rar ./temp/12/0/rar
+                    break;
+            }
+            exec(execQuery, function(err, stdout, stderr) {
+                if(err !== null)
+                    return reject(stderr);
+                return resolve(file);
+            });
         });
     };
 
